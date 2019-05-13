4fec09dfb2fda6146f6dc099ee6dab3f0bf7e562
Rafael Rodriguez <rafael.roguz@gmail.com>
2019-05-13 09:18:31 -0500

    Se añadieron cambios

--- ProyectoFinal/AsistenteInteligente/AsistenteInteligente.ino
+++ ProyectoFinal/AsistenteInteligente/AsistenteInteligente.ino
@@ -36,35 +36,32 @@ Adafruit_MQTT_Subscribe requestFeed = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "
 // Will publish updates to the feed "Temperature"
 Adafruit_MQTT_Publish temperatureFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/temperature");
 // Will publish updates to the feed "alarmOn"
-Adafruit_MQTT_Publish alarmOnFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/alarmOn");
+Adafruit_MQTT_Publish alarmFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/alarm");
 
 // The pin where the lights will be connected
-const int pinLights = 23;
+const int pinLights = 21;
 // The pin where the LED indicator will be connected
 const int pinLed = 13;
 // The pin where the buzzer will be connected
 const int pinBuzzer = 14;
 // The pin where the movement sensor will be connected
 const int pinMov = 34;
+
 // Turn on/off the alarmOn
 bool alarmOn = false;
+bool alerted = false;
 
+// The interruption code. Will turn on the lights, leds and buzzer, and will
+// raise the alarm flag on
 void IRAM_ATTR ISR() {
-    Serial.println("Alarma activada!");
+    Serial.println("Alarm ON!");
 
-    if (alarmOn == LOW) {
+    if (!alarmOn) {
       digitalWrite(pinLights, HIGH);
       digitalWrite(pinLed, HIGH);
       digitalWrite(pinBuzzer, HIGH);
-      // Connects/reconnects to MQTT
-      //MQTT_connect();
 
-      // Publish the alarmOn event on the adafruit feed "alarmOn"
-      //alarmOnFeed.publish("ON");  
-
-      alarmOn = HIGH;
-      
-      //Serial.println("Sended an alarmOn message!");
+      alarmOn = true;
     }
 }
 
@@ -82,33 +79,38 @@ void setup() {
     delay(1000);
   }
 
-  Serial.println("\nConected to Wi-Fi!");
+  Serial.println("Conected to Wi-Fi!");
 
   // Subscribe to the updates on the "request" feed
   mqtt.subscribe(&requestFeed);
 
-  // Declares the lights pin as an output
+  // Declares the output pins
   pinMode(pinLights, OUTPUT);
-  // Declares the LED pin as an output
   pinMode(pinLed, OUTPUT);
-  // Declares the buzzer pin as an output
   pinMode(pinBuzzer, OUTPUT);
-  // Declares the pin as input for movement detection
+  
+  // Declares the input pins
   pinMode(pinMov, INPUT);
+
+  // Initializes the output pins with LOW (OFF)
+  digitalWrite(pinLights, LOW);
+  digitalWrite(pinLed, LOW);
+  digitalWrite(pinBuzzer, LOW);
 
   // Beging execution of the DHT11
   dht.begin();
 
-  attachInterrupt(pinMov, ISR, RISING);
+  MQTT_connect();
+
+  // Starts the interrption monitoring for the pir sensor pin
+  attachInterrupt(digitalPinToInterrupt(pinMov), ISR, RISING);
 }
 
 void loop() { 
-  Serial.print("Alarma: "); Serial.println(alarmOn);
-  
   //Connects/Reconnects to MQTT
-  MQTT_connect();
+  //MQTT_connect();
   
-  Serial.println("\n");
+  Serial.print("Alarma: "); Serial.println(alarmOn);
 
   // Read from our subscription queue until we run out, or
   // wait up to 5 seconds for subscription to update
@@ -119,7 +121,7 @@ void loop() {
     //If we're in here, a subscription updated...
     if (subscription == &requestFeed) {
       //Print the new value to the serial monitor
-      Serial.print("Got new request: ");
+      Serial.print("\nGot new request: ");
       Serial.println((char*) requestFeed.lastread);
       Serial.println("");
 
@@ -128,10 +130,14 @@ void loop() {
         // Turn on the lights
         digitalWrite(pinLights, HIGH);
         digitalWrite(pinLed, HIGH);
-        digitalWrite(pinBuzzer, LOW);
-        alarmOn = LOW;
-      } else if (!strcmp((char*) requestFeed.lastread, "LIGHTS_OFF")) {
+
+        // Turning the AC lights will trigger the interruption, so we shut off the lights again
+        // and turn off the alarm after 1 seconds
+        delay(500);
         alarmOn = false;
+        digitalWrite(pinBuzzer, LOW);
+        
+      } else if (!strcmp((char*) requestFeed.lastread, "LIGHTS_OFF")) {
         // Turn off the lights
         digitalWrite(pinLights, LOW);
         digitalWrite(pinLed, LOW);
@@ -142,12 +148,66 @@ void loop() {
         temperatureFeed.publish(temp);
       } else if (!strcmp((char*) requestFeed.lastread, "ALARM_OFF")) {
         alarmOn = false;
+        alerted = false;
         digitalWrite(pinLights, LOW);
         digitalWrite(pinLed, LOW);
         digitalWrite(pinBuzzer, LOW);
+        delay(500);
+        alarmOn = false;
+        alerted = false;
+        digitalWrite(pinLights, LOW);
+        digitalWrite(pinLed, LOW);
+        digitalWrite(pinBuzzer, LOW);
+        
+        alarmFeed.publish("OFF"); 
+        delay(1000);
+        alarmFeed.publish("OFF"); 
+        delay(1000);
+        alarmFeed.publish("OFF"); 
       }
     }  
   }  
+
+  if (alarmOn) {
+    if (alerted == false) {
+      Serial.println("Sending alert messages...");
+      alerted = true;
+      alarmFeed.publish("ON");
+      delay(1000);
+      alarmFeed.publish("ON");
+      delay(1000);
+      alarmFeed.publish("ON");
+    }
+  }
+}
+
+// Connects to the MQTT server on the adafruit platform
+void MQTT_connect() {
+  if (mqtt.connected() && mqtt.ping()) {
+    return;
+  } 
+  
+  int8_t ret;
+
+  mqtt.disconnect();
+
+  Serial.print("Connecting to MQTT...");
+  uint8_t retries = 3;
+
+  // Tries to connecte to the MQTT server. If it fails 3 times, resets the board
+  while ((ret = mqtt.connect()) != 0) {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    
+    if (retries == 0) {
+      ESP.restart();
+    }
+  }
+  
+  Serial.println("MQTT Connected!");
 }
 
 // Gets the temperature from the DHT11 temperature sensor
@@ -178,34 +238,4 @@ float getTemperature() {
   Serial.print(" *C\n\n");
 
   return t;
-}
-
-// Connects to the MQTT server on the adafruit platform
-void MQTT_connect() {
-  // If there's already a connection, exit the function
-  if (mqtt.connected() && mqtt.ping()) {
-    return;
-  }
-
-  int8_t ret;
-
-  mqtt.disconnect();
-
-  Serial.print("Connecting to MQTT...");
-  uint8_t retries = 3;
-
-  // Tries to connecte to the MQTT server. If it fails 3 times, resets the board
-  while ((ret = mqtt.connect()) != 0) {
-    Serial.println(mqtt.connectErrorString(ret));
-    Serial.println("Retrying MQTT connection in 5 seconds...");
-    mqtt.disconnect();
-    delay(5000);  // wait 5 seconds
-    retries--;
-    
-    if (retries == 0) {
-      ESP.restart();
-    }
-  }
-  
-  Serial.println("MQTT Connected!");
 }
